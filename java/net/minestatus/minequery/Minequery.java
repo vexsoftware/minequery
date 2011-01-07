aa6afe4e6d443291a3665580ffa12b15da6ad281
Blake Beaupain <blakeman8192@hotmail.com>
2011-01-06 17:51:20 -0800

    Bukkit version of Minequery.

---
+++ java/net/minestatus/minequery/Minequery.java
@@ -0,0 +1,110 @@
+package net.minestatus.minequery;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.Server;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A port of Minequery that works with the Bukkit plugin platform.
+ * 
+ * @since 1.0
+ * @author Blake Beaupain
+ */
+public final class Minequery extends JavaPlugin {
+
+	/** The main configuration file. */
+	public static final String CONFIG_FILE = "server.properties";
+
+	/** The logging utility (used for error logging). */
+	private final Logger log = Logger.getLogger(getClass().getName());
+
+	/** The port of the Minecraft server. */
+	private int serverPort;
+
+	/** The maximum amount of players allowed on the Minecraft server. */
+	private int maxPlayers;
+
+	/** The main Minequery server. */
+	private MinequeryServer server;
+
+	/**
+	 * Creates a new <code>Minequery</code> object. This constructor is
+	 * inherited from {@link org.bukkit.plugin.java.JavaPlugin}.
+	 * 
+	 * @param pluginLoader
+	 * @param instance
+	 * @param desc
+	 * @param plugin
+	 * @param cLoader
+	 */
+	public Minequery(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File plugin, ClassLoader cLoader) {
+		super(pluginLoader, instance, desc, plugin, cLoader);
+
+		// Initialize the Minequery plugin.
+		try {
+			Properties props = new Properties();
+			props.load(new FileReader(CONFIG_FILE));
+			server = new MinequeryServer(this, Integer.parseInt(props.getProperty("minequery-port")));
+			serverPort = Integer.parseInt(props.getProperty("server-port"));
+			maxPlayers = Integer.parseInt(props.getProperty("max-players"));
+		} catch (IOException ex) {
+			log.log(Level.SEVERE, "Error initializing Minequery.", ex);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.bukkit.plugin.Plugin#onDisable()
+	 */
+	@Override
+	public void onDisable() {
+		try {
+			server.getListener().close();
+		} catch (IOException ex) {
+			log.log(Level.WARNING, "Unable to close the MinequeryServer listener.", ex);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.bukkit.plugin.Plugin#onEnable()
+	 */
+	@Override
+	public void onEnable() {
+		if (server == null) {
+			throw new IllegalStateException("Cannot enable - MinequeryServer not initialized.");
+		}
+
+		// Start the server normally.
+		server.start();
+	}
+
+	/**
+	 * Gets the port that the Minecraft server is running on.
+	 * 
+	 * @return The Minecraft server port
+	 */
+	public int getServerPort() {
+		return serverPort;
+	}
+
+	/**
+	 * Gets the maximum amount of players the Minecraft server can hold.
+	 * 
+	 * @return The maximum amount of players
+	 */
+	public int getMaxPlayers() {
+		return maxPlayers;
+	}
+
+}
---
+++ java/net/minestatus/minequery/MinequeryServer.java
@@ -0,0 +1,122 @@
+package net.minestatus.minequery;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The main networking hub that listens for and responds to Minequery requests.
+ * 
+ * @since 1.0
+ * @author Blake Beaupain
+ */
+public final class MinequeryServer extends Thread {
+
+	/** The parent plugin object. */
+	private final Minequery minequery;
+
+	/** The MinequeryServer port. */
+	private final int port;
+
+	/** The connection listener. */
+	private final ServerSocket listener;
+
+	/** The logging utility. */
+	private final Logger log = Logger.getLogger(getClass().getName());
+
+	/**
+	 * Creates a new <code>MinequeryServer</code> object.
+	 * 
+	 * @param minequery
+	 *            The parent plugin object
+	 * @param port
+	 *            The port that this server will run on
+	 * @throws IOException
+	 *             If an I/O error occurs
+	 */
+	public MinequeryServer(Minequery minequery, int port) throws IOException {
+		this.minequery = minequery;
+		this.port = port;
+		listener = new ServerSocket(port);
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				// Wait for and accept all incoming connections.
+				Socket socket = getListener().accept();
+
+				// Read the request and handle it.
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				handleRequest(socket, reader.readLine());
+				socket.close();
+			}
+		} catch (IOException ex) {
+			log.log(Level.SEVERE, "Minequery server thread shutting down.", ex);
+		}
+	}
+
+	/**
+	 * Handles a received request.
+	 * 
+	 * @param request
+	 *            The request message
+	 * @throws IOException
+	 *             If an I/O error occurs
+	 */
+	private void handleRequest(Socket socket, String request) throws IOException {
+		// Handle a query request.
+		if (request.equalsIgnoreCase("QUERY")) {
+			Minequery m = getMinequery();
+
+			// Build the response.
+			StringBuilder resp = new StringBuilder();
+			resp.append("SERVERPORT " + m.getServerPort() + "\n");
+			resp.append("PLAYERCOUNT " + m.getServer().getOnlinePlayers().length + "\n");
+			resp.append("MAXPLAYERS " + m.getMaxPlayers());
+			resp.append("PLAYERLIST " + Arrays.toString(m.getServer().getOnlinePlayers()));
+
+			// Send the response.
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			writer.write(resp.toString());
+		}
+
+		// Different requests may be introduced in the future.
+	}
+
+	/**
+	 * Gets the <code>Minequery</code> parent plugin object.
+	 * 
+	 * @return The Minequery object
+	 */
+	public Minequery getMinequery() {
+		return minequery;
+	}
+
+	/**
+	 * Gets the <code>MinequeryServer</code> port.
+	 * 
+	 * @return The port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * Gets the listening <code>ServerSocket</code>.
+	 * 
+	 * @return The server socket
+	 */
+	public ServerSocket getListener() {
+		return listener;
+	}
+
+}
